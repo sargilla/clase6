a326b8dd6ef07d3cccdab14c1c7e6f295a6e5049
Santiago Argilla <sargilla@gmail.com>
2020-04-06 17:25:57 -0300

    mejorando el router hard

--- hard/app.js
+++ hard/app.js
@@ -1,9 +1,11 @@
-const http = require('http');
+const http = require("http");
 
-const router = require('./router');
+const router = require("./router");
 
-http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-	// Route System
-	router.routes(req, res)
-}).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    // Route System
+    router(req, res);
+  })
+  .listen(3030, "localhost", () => console.log("Server running in 3030 port"));
--- hard/router.js
+++ hard/router.js
@@ -1,35 +1,33 @@
-const contenido = require('./src');
+const contenido = require("./src");
 
-module.exports = {
-    routes: (req, res) => {
-        switch (req.url) {
-            // Home
-            case '/':
-                res.end(contenido.home);
-                break;
-            // En cartelera
-            case '/en-cartelera':
-                res.end(contenido.cartelera);
-                break;
-            // Mas Votadas
-            case '/mas-votadas':
-                res.end(contenido.masVotadas);
-                break;
-            // Sucursales
-            case '/sucursales':
-                res.end(contenido.sucursales);
-                break;
-            // Contacto
-            case '/contacto':
-                res.end(contenido.contacto);
-                break;
-            // Preguntas Frecuentes
-            case '/preguntas-frecuentes':
-                res.end(contenido.faq);
-                break;
-            // Default
-            default:
-                res.end(contenido.notFound);
-        }
-    }
-}
+module.exports = (req, res) => {
+  switch (req.url) {
+    // Home
+    case "/":
+      res.end(contenido.home);
+      break;
+    // En cartelera
+    case "/en-cartelera":
+      res.end(contenido.cartelera);
+      break;
+    // Mas Votadas
+    case "/mas-votadas":
+      res.end(contenido.masVotadas);
+      break;
+    // Sucursales
+    case "/sucursales":
+      res.end(contenido.sucursales);
+      break;
+    // Contacto
+    case "/contacto":
+      res.end(contenido.contacto);
+      break;
+    // Preguntas Frecuentes
+    case "/preguntas-frecuentes":
+      res.end(contenido.faq);
+      break;
+    // Default
+    default:
+      res.end(contenido.notFound);
+  }
+};
