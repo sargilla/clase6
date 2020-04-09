e043048f43ea8bffedb1e56bfd52b5ccaa8199b0
Santiago Argilla <sargilla@gmail.com>
2020-04-09 19:39:10 -0300

    uno mas

--- hard/src/enCartelera.js
+++ hard/src/enCartelera.js
@@ -11,13 +11,10 @@ for (let movie of movies) {
     finalContent += `Título: ${movie.title}\nReseña: ${movie.overview}\n\n`;
 }
 
-finalContent += '\n' + '='.repeat(80) + '\n';
+finalContent += '\n' + '='.repeat(80) + '\n\n';
 
 finalContent += `\nRecordá que podés visitar las secciones:\n\n• Inicio (/)\n• En Cartelera (/en-cartelera)\n• Más Votadas (/mas-votadas)\n• Sucursales (/sucursales)\n• Contacto (/contacto)\n• Preguntas Frecuentes (/preguntas-frecuentes)`;
 
 finalContent += '\n\n' + '='.repeat(80) + '\n';
 
 module.exports = finalContent;
-
-
-
